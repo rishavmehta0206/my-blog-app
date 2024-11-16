da03dbc0b8e50125e41e251c65be7f8e355efbd9
rishavmehta <rishav.mehta0206@gmail.com>
2024-11-17 02:12:22 +0530

    first commit

---
+++ src/app/layout.js
@@ -0,0 +1,31 @@
+import { Inter } from "next/font/google";
+import "./globals.css";
+import Navbar from "@/components/navbar/Navbar";
+import Footer from "@/components/footer/Footer";
+import ClientSideProviderTest from "@/components/clientSideProviderTest";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata = {
+  title: {
+    default:"Blog",
+    template:""
+  },
+  description: "This is a next blog app",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        {/* <ClientSideProviderTest> */}
+          <div className="container">
+            <Navbar />
+            {children}
+            <Footer />
+          </div>
+        {/* </ClientSideProviderTest> */}
+      </body>
+    </html>
+  );
+}
