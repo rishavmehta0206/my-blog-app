61260500cb053ed43eceb2491afe5e5181137ba6
rishavmehta <rishav.mehta0206@gmail.com>
2024-11-18 00:45:26 +0530

    fixed bugs

--- src/app/navigationtest/page.jsx
+++ src/app/navigationtest/page.jsx
@@ -5,23 +5,23 @@ import { usePathname, useRouter, useSearchParams } from "next/navigation"
 const NavigationTestPage = () => {
 
   // CLIENT SIDE NAVIGATION
-  const router = useRouter()
-  const pathname = usePathname()
-  const searchParams = useSearchParams()
+  // const router = useRouter()
+  // const pathname = usePathname()
+  // const searchParams = useSearchParams()
 
-  const q = searchParams.get("q")
+  // const q = searchParams.get("q")
 
-  console.log(q)
+  // console.log(q)
 
-  const handleClick = ()=>{
-    console.log("clicked")
-    router.forward()
-  }
+  // const handleClick = ()=>{
+  //   console.log("clicked")
+  //   router.forward()
+  // }
 
   return (
     <div>
-      <Link href="/" prefetch={false}>Click here</Link>
-      <button onClick={handleClick}>Write and Redirect</button>
+      {/* <Link href="/" prefetch={false}>Click here</Link>
+      <button onClick={handleClick}>Write and Redirect</button> */}
     </div>
   )
 }
